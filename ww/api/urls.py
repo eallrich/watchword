39ca2b6000cdc9f61914958d707b2b25f10984c1
Evan Allrich <evan@unguku.com>
2016-02-14 18:52:30 -0600

    Be specific with acceptable URLs

--- ww/api/urls.py
+++ ww/api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from ww.api import views
 
 urlpatterns = [
-    url(r'^ping/([0-9a-fA-F]+)', views.ping, name='ww-api-ping'),
-    url(r'^status/([0-9a-fA-F]+)', views.status, name='ww-api-status'),
+    url(r'^ping/([0-9a-fA-F]{10})/?$', views.ping, name='ww-api-ping'),
+    url(r'^status/([0-9a-fA-F]{10})/?$', views.status, name='ww-api-status'),
 ]
