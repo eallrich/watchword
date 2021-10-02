99df8cd597fb9b0d5187c2e6433b09664b850a5f
Evan Allrich <git@allrich.dev>
2021-10-02 12:16:33 -0500

    Update settings for Django 3.2
    - The SessionAuthenticationMiddleware no longer exists
    - Integrate Whitenoise via middleware instead of a WSGI wrapper
    - Reference the admin URLs directly instead of `include`ing  them

--- ww/urls.py
+++ ww/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^hq/', include(admin.site.urls)),
+    url(r'^hq/', admin.site.urls),
     url(r'^',    include('ww.api.urls')),
     url(r'^',    include('ww.frontend.urls')),
 ]
